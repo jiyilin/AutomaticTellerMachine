9341147effe8e86476ebb5da93d0e504946d4baf
jiyilin <2474803745@qq.com>
2022-12-09 01:08:55 +0800

    finsh show

--- AdminSide.cpp
+++ AdminSide.cpp
@@ -3,6 +3,9 @@
 AdminSide::AdminSide(QWidget *parent): QMainWindow(parent)
 {
 	ui.setupUi(this);
+	ui.LoadWindow->hide();
+	ui.MenuWindow->hide();
+	ui.LoadDashboardWindow->hide();
 }
 
 AdminSide::~AdminSide() = default;
