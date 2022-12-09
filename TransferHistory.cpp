016444bcaf3dc97cd014500fdcb34fffb75de190
jiyilin <2474803745@qq.com>
2022-12-10 02:04:48 +0800

    finsh TranserHistory_H

---
+++ TransferHistory.cpp
@@ -0,0 +1,37 @@
+#include<time.h>
+#include<sstream>
+#include "TransferHistory.h"
+
+TransferHistory::TransferHistory(std::string& lock, double cash)
+{
+	this->transferred = lock;
+	this->amount = cash;
+	time_t now = time(nullptr);
+	tm* process = localtime(&now);
+	std::stringstream stream;
+	stream << process->tm_year + 1900 << "_" << process->tm_mon + 1 << "_" << process->tm_mday <<
+				"_" << process->tm_hour << "_" << process->tm_min << "_" << process->tm_sec;
+	stream >> this->transferredTime;
+}
+
+TransferHistory::TransferHistory(std::string& lock, double cash, std::string &lockTime)
+{
+	this->transferred = lock;
+	this->amount = cash;
+	this->transferredTime = lockTime;
+}
+
+std::string TransferHistory::Gain_transferred()
+{
+	return this->transferred;
+}
+
+double TransferHistory::Gain_amount()
+{
+	return this->amount;
+}
+
+std::string TransferHistory::Gain_transferredTime()
+{
+	return this->transferredTime;
+}
---
+++ TransferHistory.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#ifndef TranserHistory_H
+#define TranserHistory_H
+
+#include<string>
+
+class TransferHistory
+{
+private:
+	std::string transferred;
+	double amount;
+	std::string transferredTime;
+public:
+	TransferHistory(std::string &lock,double cash);
+	TransferHistory(std::string& lock, double cash, std::string &lockTime);
+	std::string Gain_transferred();
+	double Gain_amount();
+	std::string Gain_transferredTime();
+};
+
+#endif // !TranserHistory_H
